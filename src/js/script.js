87201dabc4073b92dea368722b3bc47d5dce1121
Davi Damasceno de Sousa <159800322+davitkblade@users.noreply.github.com>
2024-09-20 07:32:55 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 const alienForm = document.getElementById('alien-form')
-//Seleciona o elemento HTML com o ID 'alien-form' e armazena na variável 'alienForm'.
+
 const alienInfo = document.getElementById('alien-info')
 
-// Cria um array chamado 'aliens' com informações sobre cada alienígena.
+
 const aliens = [
   { name: 'Chama', description: 'Chama é a amostra de DNA do Omnitrix de um Pyronita da estrela Pyros.' }, 
   { name: 'Besta', description: 'Besta é a amostra de DNA do Omnitrix de um Vulpimancer do planeta Vulpin.' },
@@ -15,7 +15,6 @@ const aliens = [
   { name: 'Ultra-T', description: 'Ultra T é a amostra de DNA do Omnitrix de um Mecamorfo galvânico da lua Galvan B.' },
   { name: 'Fantasmático', description: 'Fantasmático é a amostra de DNA do Omnitrix de um Ectonurita do planeta Anur Phaetos.' },
 ]
-// Cria um array chamado 'alienUrls' com os URLs de cada alienígena.
 const alienUrls = [
   "./src/all.html/chama.html",
   "./src/all.html/besta.html",
@@ -29,13 +28,13 @@ const alienUrls = [
   "./src/all.html/fantasmatico.html",
 ]
 
-// Define a função 'handleAlienSelection' que é chamada quando o usuário seleciona um alienígena.
+
 function handleAlienSelection(event) {
-  const alienNumber = parseInt(document.getElementById('alien-number').value); // Obtem o valor do campo de entrada 'alien-number' e converte para um inteiro.
-  if (alienNumber >= 1 && alienNumber <= alienUrls.length) { // Verifica se o número do alienígena é válido (entre 1 e o tamanho do array 'alienUrls').
-    const url = alienUrls[alienNumber - 1] // Obtém o URL do alienígena correspondente ao número selecionado.
-    window.location.href = url // Redireciona o usuário para o URL do alienígena selecionado.
+  const alienNumber = parseInt(document.getElementById('alien-number').value)
+  if (alienNumber >= 1 && alienNumber <= alienUrls.length) {
+    const url = alienUrls[alienNumber - 1] .
+    window.location.href = url 
   } else {
-    alert("Número inválido. Digite um número entre 1 e " + alienUrls.length) // Exibe uma mensagem de alerta caso o número do alienígena seja inválido.
+    alert("Número inválido. Digite um número entre 1 e " + alienUrls.length)
   }
 }
